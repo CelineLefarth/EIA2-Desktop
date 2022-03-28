e40ffc3b768b3ea742fe9012caac44523aab4a9c
Woschny <92034289+Woschny@users.noreply.github.com>
2022-03-28 20:21:27 +0200

    Aufgaben Poem und Boxes korrigiert

--- Aufgaben/01_RandomPoem/RPScriptWithParameter.js
+++ Aufgaben/01_RandomPoem/RPScriptWithParameter.js
@@ -1,32 +1,32 @@
 var randomPoem;
 (function (randomPoem) {
     window.addEventListener("load", handleLoad);
-    let subject = ["C3PO", "R2D2", "Ein kleiner Ewok", "Ein großer Wookiee", "Yoda", "Jar Jar Binks"];
-    let predicate = ["tötet", "umarmt", "beobachtet", "springt auf", "erwürgt", "umklammert"];
-    let objekt = ["General Grievous", "Count Dooku", "Darth Plagueis", "Boba Fett", "Jabba Desilijic Tiure", "Asajj Ventress"];
-    let subjectCopy = [];
-    let predicateCopy = [];
-    let objectCopy = [];
+    let subjects = ["C3PO", "R2D2", "Ein kleiner Ewok", "Ein großer Wookiee", "Yoda", "Jar Jar Binks"];
+    let predicates = ["tötet", "umarmt", "beobachtet", "springt auf", "erwürgt", "umklammert"];
+    let objekts = ["General Grievous", "Count Dooku", "Darth Plagueis", "Boba Fett", "Jabba Desilijic Tiure", "Asajj Ventress"];
+    let subjectsCopy = [];
+    let predicatesCopy = [];
+    let objectsCopy = [];
+    subjectsCopy = subjects.concat([]);
+    predicatesCopy = predicates.concat([]);
+    objectsCopy = objekts.concat([]);
+    function getRandom() {
+        return Math.floor(Math.random() * subjectsCopy.length);
+    }
+    function getVerse(_subjects, _predicates, _objects) {
+        let subIndex = getRandom();
+        let preIndex = getRandom();
+        let objIndex = getRandom();
+        let line = _subjects[subIndex] + " " + _predicates[preIndex] + " " + _objects[objIndex];
+        document.querySelector("span").innerHTML += line + "<br>";
+        _subjects.splice(subIndex, 1);
+        _predicates.splice(preIndex, 1);
+        _objects.splice(objIndex, 1);
+        return line;
+    }
     function handleLoad() {
-        subjectCopy = subject.concat([]);
-        predicateCopy = predicate.concat([]);
-        objectCopy = objekt.concat([]);
-        function getRandom() {
-            return Math.floor(Math.random() * subjectCopy.length);
-        }
-        function getVerse(_subject, _predicate, _object) {
-            let subIndex = getRandom();
-            let preIndex = getRandom();
-            let objIndex = getRandom();
-            let line = _subject[subIndex] + " " + _predicate[preIndex] + " " + _object[objIndex];
-            document.querySelector("span").innerHTML += line + "<br>";
-            _subject.splice(subIndex, 1);
-            _predicate.splice(preIndex, 1);
-            _object.splice(objIndex, 1);
-            return line;
-        }
-        for (let i = subject.length; i > 0; i--) {
-            let printLine = getVerse(subjectCopy, predicateCopy, objectCopy);
+        for (let i = subjects.length; i > 0; i--) {
+            let printLine = getVerse(subjectsCopy, predicatesCopy, objectsCopy);
             console.log(printLine);
         }
     }
--- Aufgaben/01_RandomPoem/RPScriptWithParameter.ts
+++ Aufgaben/01_RandomPoem/RPScriptWithParameter.ts
@@ -1,43 +1,43 @@
 namespace randomPoem {
-window.addEventListener("load", handleLoad);
+    window.addEventListener("load", handleLoad);
 
-let subject: string[] = ["C3PO", "R2D2", "Ein kleiner Ewok", "Ein großer Wookiee", "Yoda", "Jar Jar Binks"];
-let predicate: string[] = ["tötet", "umarmt", "beobachtet", "springt auf", "erwürgt", "umklammert"];
-let objekt: string[] = ["General Grievous", "Count Dooku", "Darth Plagueis", "Boba Fett", "Jabba Desilijic Tiure", "Asajj Ventress"];
+    let subjects: string[] = ["C3PO", "R2D2", "Ein kleiner Ewok", "Ein großer Wookiee", "Yoda", "Jar Jar Binks"];
+    let predicates: string[] = ["tötet", "umarmt", "beobachtet", "springt auf", "erwürgt", "umklammert"];
+    let objekts: string[] = ["General Grievous", "Count Dooku", "Darth Plagueis", "Boba Fett", "Jabba Desilijic Tiure", "Asajj Ventress"];
 
-let subjectCopy: string[] = [];
-let predicateCopy: string[] = [];
-let objectCopy: string[] = [];
+    let subjectsCopy: string[] = [];
+    let predicatesCopy: string[] = [];
+    let objectsCopy: string[] = [];
 
-function handleLoad(): void {
 
-    subjectCopy = subject.concat([]);
-    predicateCopy = predicate.concat([]);
-    objectCopy = objekt.concat([]);
+    subjectsCopy = subjects.concat([]);
+    predicatesCopy = predicates.concat([]);
+    objectsCopy = objekts.concat([]);
 
     function getRandom(): number {
-        return Math.floor(Math.random() * subjectCopy.length);
+        return Math.floor(Math.random() * subjectsCopy.length);
     }
 
-    function getVerse(_subject: string[], _predicate: string[], _object: string[]): string {
+    function getVerse(_subjects: string[], _predicates: string[], _objects: string[]): string {
 
         let subIndex: number = getRandom();
         let preIndex: number = getRandom();
         let objIndex: number = getRandom();
 
-        let line: string = _subject[subIndex] + " " + _predicate[preIndex] + " " + _object[objIndex];
+        let line: string = _subjects[subIndex] + " " + _predicates[preIndex] + " " + _objects[objIndex];
         document.querySelector("span").innerHTML += line + "<br>";
 
-        _subject.splice(subIndex, 1);
-        _predicate.splice(preIndex, 1);
-        _object.splice(objIndex, 1);
+        _subjects.splice(subIndex, 1);
+        _predicates.splice(preIndex, 1);
+        _objects.splice(objIndex, 1);
 
         return line;
     }
 
-    for (let i: number = subject.length; i > 0; i--) {
-        let printLine: string = getVerse(subjectCopy, predicateCopy, objectCopy);
-        console.log(printLine);
+    function handleLoad(): void {
+        for (let i: number = subjects.length; i > 0; i--) {
+            let printLine: string = getVerse(subjectsCopy, predicatesCopy, objectsCopy);
+            console.log(printLine);
+        }
     }
-}
 }
