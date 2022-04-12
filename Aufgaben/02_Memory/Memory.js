08afe3781a09351413ae6cb12aaedfc134cbf124
Woschny <92034289+Woschny@users.noreply.github.com>
2022-04-12 19:38:07 +0200

    Memory03

--- Aufgaben/02_Memory/Memory.js
+++ Aufgaben/02_Memory/Memory.js
@@ -1,29 +1,176 @@
 var Memory;
 (function (Memory) {
+    /*
+    Aufgabe: <02.2 Memory>
+    Name: <Silvan Woschny>
+    Matrikel: <	269370>
+    Datum: <12.04.2022>
+    Quellen: <>
+    */
     window.addEventListener("load", handleLoad);
+    //Game variables
     let seconds = 0;
     let minutes = 0;
     let clicked = 0;
     let toCheck = [];
+    //Default values
+    let cardSize = 40;
+    let cardFont = "";
+    //All menu options
+    const stepper = document.createElement("input");
+    const delMenu = document.createElement("button");
+    const sizeSlider = document.createElement("input");
+    const sizeAmount = document.createElement("span");
+    const colorSelectBG = document.createElement("input");
+    const colorSelectFont = document.createElement("input");
+    const colorSelectCard = document.createElement("input");
+    //Menu <-------------------------------------------------------------------->
     function handleLoad() {
-        createPrompt();
+        startMenu();
     }
-    function createPrompt() {
-        const amount = prompt("Mit wie vielen Karten willst du spielen? Denk daran, dass es eine gerade Zahl zwischen 10 und 50 sein muss!", "18");
-        let cardAmount = parseInt(amount);
-        if (cardAmount % 2 == 0 && cardAmount <= 50 && cardAmount >= 10) {
-            createCards(cardAmount);
+    function startMenu() {
+        changeCardNumber();
+        changeCardSize();
+        changeCardColor();
+        changeCardFont();
+        delStartMenu();
+    }
+    //Number-Stepper in menu
+    function changeCardNumber() {
+        const label = document.createElement("label");
+        stepper.setAttribute("type", "number");
+        stepper.setAttribute("value", "18");
+        stepper.setAttribute("min", "10");
+        stepper.setAttribute("max", "50");
+        stepper.setAttribute("step", "2");
+        label.innerHTML = "Kartenanzahl: ";
+        document.querySelector("form").appendChild(label);
+        document.querySelector("form").appendChild(stepper);
+    }
+    //Card-Size in menu
+    function changeCardSize() {
+        const label = document.createElement("label");
+        sizeSlider.setAttribute("type", "range");
+        sizeSlider.setAttribute("min", "5");
+        sizeSlider.setAttribute("max", "50");
+        sizeSlider.setAttribute("step", "1");
+        sizeSlider.setAttribute("value", "40");
+        label.innerHTML = "<br>" + "Kartengröße: ";
+        document.querySelector("form").appendChild(label);
+        document.querySelector("form").appendChild(sizeSlider);
+        document.querySelector("form").appendChild(sizeAmount);
+        sizeAmount.innerHTML = "40";
+        sizeSlider.addEventListener("input", displayAmount);
+    }
+    function displayAmount(_event) {
+        let size = sizeSlider.value;
+        if (parseInt(size) < 10) {
+            sizeAmount.innerHTML = "0" + size;
         }
         else {
-            createPrompt();
+            sizeAmount.innerHTML = size;
+        }
+        cardSize = parseInt(size);
+    }
+    //Card-Colors in menu
+    function changeCardColor() {
+        changeBgCardColor();
+        changeFontCardColor();
+        changeCardCardColor();
+    }
+    function changeBgCardColor() {
+        const labelBG = document.createElement("label");
+        document.querySelector("form").appendChild(labelBG);
+        labelBG.innerHTML = "<br>" + "Hintergrundfarbe: ";
+        colorSelectBG.setAttribute("type", "color");
+        colorSelectBG.setAttribute("id", "bg");
+        colorSelectBG.setAttribute("value", "#ffffff");
+        labelBG.setAttribute("for", "bg");
+        document.querySelector("form").appendChild(colorSelectBG);
+    }
+    function changeFontCardColor() {
+        const labelFont = document.createElement("label");
+        document.querySelector("form").appendChild(labelFont);
+        labelFont.innerHTML = "<br>" + "Fontfarbe: ";
+        colorSelectFont.setAttribute("type", "color");
+        colorSelectFont.setAttribute("id", "font");
+        colorSelectFont.setAttribute("value", "#000000");
+        labelFont.setAttribute("for", "font");
+        document.querySelector("form").appendChild(colorSelectFont);
+    }
+    function changeCardCardColor() {
+        const labelCard = document.createElement("label");
+        document.querySelector("form").appendChild(labelCard);
+        labelCard.innerHTML = "<br>" + "Kartenfarbe: ";
+        colorSelectCard.setAttribute("type", "color");
+        colorSelectCard.setAttribute("id", "card");
+        colorSelectCard.setAttribute("value", "#000000");
+        labelCard.setAttribute("for", "card");
+        document.querySelector("form").appendChild(colorSelectCard);
+    }
+    //Card-Font in menu
+    function changeCardFont() {
+        const fontButton1 = document.createElement("input");
+        const label1 = document.createElement("label");
+        label1.style.fontFamily = "monospace";
+        label1.style.fontWeight = "900";
+        const fontButton2 = document.createElement("input");
+        const label2 = document.createElement("label");
+        label2.style.fontFamily = "sand-serif";
+        const fontButton3 = document.createElement("input");
+        const label3 = document.createElement("label");
+        label3.style.fontFamily = "Lucida Sans";
+        let allFontButtons = [fontButton1, fontButton2, fontButton3];
+        for (let i = 0; i <= 2; i++) {
+            allFontButtons[i].setAttribute("type", "radio");
+            allFontButtons[i].setAttribute("name", "radio");
+            allFontButtons[i].setAttribute("id", "radio" + i);
+            if (i == 0) {
+                allFontButtons[i].value = "monospace";
+                allFontButtons[i].setAttribute("checked", "checked");
+                label1.setAttribute("for", "radio" + i);
+                label1.innerHTML = "<br>" + allFontButtons[i].value + "";
+                document.querySelector("form").appendChild(label1);
+            } //Default Font
+            if (i == 1) {
+                allFontButtons[i].value = "sans-serif";
+                label2.setAttribute("for", "radio" + i);
+                label2.innerHTML = "<br>" + allFontButtons[i].value + "";
+                document.querySelector("form").appendChild(label2);
+            }
+            if (i == 2) {
+                allFontButtons[i].value = "Lucida Sans";
+                label3.setAttribute("for", "radio" + i);
+                label3.innerHTML = "<br>" + allFontButtons[i].value + "";
+                document.querySelector("form").appendChild(label3);
+            }
+            document.querySelector("form").appendChild(allFontButtons[i]);
         }
     }
+    function delStartMenu() {
+        document.querySelector("form").appendChild(delMenu);
+        delMenu.innerHTML = "Start";
+        delMenu.addEventListener("click", startGame);
+    }
+    function startGame() {
+        radioEntry();
+        document.querySelector("body").style.backgroundColor = colorSelectBG.value;
+        document.querySelector("body").removeChild(document.querySelector("form"));
+        createCards(parseInt(stepper.value));
+    }
+    function radioEntry() {
+        let formData = new FormData(document.forms[0]);
+        if (formData.get("radio") != null && formData.get("radio") != undefined) {
+            cardFont = (formData.get("radio")) + "";
+        }
+    }
+    //Game <-------------------------------------------------------------------->
     function createCards(_amount) {
         let cards = [];
         for (let i = _amount; i > 0; i--) {
             if (i % 2 == 0) {
-                cards.push(i); //Karte 1
-                cards.push(i); //Karte 2
+                cards.push(i); //card 1
+                cards.push(i); //card 2
             }
         }
         shuffleCards(cards);
@@ -39,9 +186,13 @@ var Memory;
     function playCard(_cards, _randomNumber) {
         let card = document.createElement("div");
         card.className = _cards[_randomNumber] + "";
+        card.style.padding = cardSize + "px";
+        card.style.fontFamily = cardFont + "";
+        card.style.color = colorSelectCard.value;
+        card.style.backgroundColor = colorSelectCard.value;
         document.querySelector("#wrapper").appendChild(card);
         card.addEventListener("click", clickCard);
-        card.setAttribute("hidden", "true");
+        card.style.backgroundColor = colorSelectCard.value;
         if (_cards[_randomNumber] < 10) {
             card.innerHTML = "0" + _cards[_randomNumber] + "";
         }
@@ -56,7 +207,8 @@ var Memory;
         }
         clicked++;
         setTimeout(click, 2000);
-        this.setAttribute("hidden", "false");
+        this.style.backgroundColor = "lightblue";
+        this.style.color = colorSelectFont.value;
         toCheck.push(this.className);
     }
     function click() {
@@ -88,13 +240,21 @@ var Memory;
         let elementsCheckOne = document.getElementsByClassName(_checkOne + "");
         elementsCheckOne[0].removeAttribute("noninteraction");
         elementsCheckOne[1].removeAttribute("noninteraction");
-        elementsCheckOne[0].setAttribute("hidden", "true");
-        elementsCheckOne[1].setAttribute("hidden", "true");
+        let elOne1 = elementsCheckOne[0];
+        elOne1.style.backgroundColor = colorSelectCard.value;
+        elOne1.style.color = colorSelectCard.value;
+        let elOne2 = elementsCheckOne[1];
+        elOne2.style.backgroundColor = colorSelectCard.value;
+        elOne2.style.color = colorSelectCard.value;
         let elementsCheckTwo = document.getElementsByClassName(_checkTwo + "");
         elementsCheckTwo[0].removeAttribute("noninteraction");
         elementsCheckTwo[1].removeAttribute("noninteraction");
-        elementsCheckTwo[0].setAttribute("hidden", "true");
-        elementsCheckTwo[1].setAttribute("hidden", "true");
+        let elTwo1 = elementsCheckTwo[0];
+        elTwo1.style.backgroundColor = colorSelectCard.value;
+        elTwo1.style.color = colorSelectCard.value;
+        let elTwo2 = elementsCheckTwo[1];
+        elTwo2.style.backgroundColor = colorSelectCard.value;
+        elTwo2.style.color = colorSelectCard.value;
     }
     function startTimer() {
         setInterval(timer, 1000);
--- Aufgaben/02_Memory/Memory.ts
+++ Aufgaben/02_Memory/Memory.ts
@@ -1,46 +1,205 @@
 namespace Memory {
 
-/*
-Aufgabe: <02.2 Memory>
-Name: <Silvan Woschny>
-Matrikel: <	269370>
-Datum: <07.04.2022>
-Quellen: <>
-*/
+    /*
+    Aufgabe: <02.2 Memory>
+    Name: <Silvan Woschny>
+    Matrikel: <	269370>
+    Datum: <12.04.2022>
+    Quellen: <>
+    */
 
     window.addEventListener("load", handleLoad);
+
+    //Game variables
     let seconds: number = 0;
     let minutes: number = 0;
     let clicked: number = 0;
     let toCheck: number[] = [];
 
+    //Default values
+    let cardSize: number = 40;
+    let cardFont: string = "";
+
+    //All menu options
+    const stepper: HTMLInputElement = document.createElement("input");
+    const delMenu: HTMLElement = document.createElement("button");
+    const sizeSlider: HTMLInputElement = document.createElement("input");
+    const sizeAmount: HTMLElement = document.createElement("span");
+    const colorSelectBG: HTMLInputElement = document.createElement("input");
+    const colorSelectFont: HTMLInputElement = document.createElement("input");
+    const colorSelectCard: HTMLInputElement = document.createElement("input");
 
 
+    //Menu <-------------------------------------------------------------------->
     function handleLoad(): void {
-        createPrompt();
+        startMenu();
+    }
+    function startMenu(): void {
+        changeCardNumber();
+        changeCardSize();
+        changeCardColor();
+        changeCardFont();
+        delStartMenu();
     }
 
 
+    //Number-Stepper in menu
+    function changeCardNumber(): void {
+        const label: HTMLElement = document.createElement("label");
+        stepper.setAttribute("type", "number");
+        stepper.setAttribute("value", "18");
+        stepper.setAttribute("min", "10");
+        stepper.setAttribute("max", "50");
+        stepper.setAttribute("step", "2");
+        label.innerHTML = "Kartenanzahl: ";
+        document.querySelector("form").appendChild(label);
+        document.querySelector("form").appendChild(stepper);
+    }
 
-    function createPrompt(): void {
-        const amount: string = prompt("Mit wie vielen Karten willst du spielen? Denk daran, dass es eine gerade Zahl zwischen 10 und 50 sein muss!", "18");
-        let cardAmount: number = parseInt(amount);
-        if (cardAmount % 2 == 0 && cardAmount <= 50 && cardAmount >= 10) {
-            createCards(cardAmount);
+
+    //Card-Size in menu
+    function changeCardSize(): void {
+        const label: HTMLElement = document.createElement("label");
+        sizeSlider.setAttribute("type", "range");
+        sizeSlider.setAttribute("min", "5");
+        sizeSlider.setAttribute("max", "50");
+        sizeSlider.setAttribute("step", "1");
+        sizeSlider.setAttribute("value", "40");
+        label.innerHTML = "<br>" + "Kartengröße: ";
+        document.querySelector("form").appendChild(label);
+        document.querySelector("form").appendChild(sizeSlider);
+        document.querySelector("form").appendChild(sizeAmount);
+        sizeAmount.innerHTML = "40";
+        sizeSlider.addEventListener("input", displayAmount);
+    }
+    function displayAmount(_event: Event): void {
+        let size: string = sizeSlider.value;
+        if (parseInt(size) < 10) {
+            sizeAmount.innerHTML = "0" + size;
         }
         else {
-            createPrompt();
+            sizeAmount.innerHTML = size;
+        }
+        cardSize = parseInt(size);
+    }
+
+
+    //Card-Colors in menu
+    function changeCardColor(): void {
+        changeBgCardColor();
+        changeFontCardColor();
+        changeCardCardColor();
+    }
+    function changeBgCardColor(): void {
+        const labelBG: HTMLElement = document.createElement("label");
+        document.querySelector("form").appendChild(labelBG);
+        labelBG.innerHTML = "<br>" + "Hintergrundfarbe: ";
+        colorSelectBG.setAttribute("type", "color");
+        colorSelectBG.setAttribute("id", "bg");
+        colorSelectBG.setAttribute("value", "#ffffff");
+        labelBG.setAttribute("for", "bg");
+        document.querySelector("form").appendChild(colorSelectBG);
+    }
+    function changeFontCardColor(): void {
+        const labelFont: HTMLElement = document.createElement("label");
+        document.querySelector("form").appendChild(labelFont);
+        labelFont.innerHTML = "<br>" + "Fontfarbe: ";
+        colorSelectFont.setAttribute("type", "color");
+        colorSelectFont.setAttribute("id", "font");
+        colorSelectFont.setAttribute("value", "#000000");
+        labelFont.setAttribute("for", "font");
+        document.querySelector("form").appendChild(colorSelectFont);
+    }
+    function changeCardCardColor(): void {
+        const labelCard: HTMLElement = document.createElement("label");
+        document.querySelector("form").appendChild(labelCard);
+        labelCard.innerHTML = "<br>" + "Kartenfarbe: ";
+        colorSelectCard.setAttribute("type", "color");
+        colorSelectCard.setAttribute("id", "card");
+        colorSelectCard.setAttribute("value", "#000000");
+        labelCard.setAttribute("for", "card");
+        document.querySelector("form").appendChild(colorSelectCard);
+    }
+
+
+    //Card-Font in menu
+    function changeCardFont(): void {
+        const fontButton1: HTMLInputElement = document.createElement("input");
+        const label1: HTMLElement = document.createElement("label");
+        label1.style.fontFamily = "monospace";
+        label1.style.fontWeight = "900";
+        const fontButton2: HTMLInputElement = document.createElement("input");
+        const label2: HTMLElement = document.createElement("label");
+        label2.style.fontFamily = "sand-serif";
+        const fontButton3: HTMLInputElement = document.createElement("input");
+        const label3: HTMLElement = document.createElement("label");
+        label3.style.fontFamily = "Lucida Sans";
+        let allFontButtons: HTMLInputElement[] = [fontButton1, fontButton2, fontButton3];
+        for (let i: number = 0; i <= 2; i++) {
+            allFontButtons[i].setAttribute("type", "radio");
+            allFontButtons[i].setAttribute("name", "radio");
+            allFontButtons[i].setAttribute("id", "radio" + i);
+            if (i == 0) {
+                allFontButtons[i].value = "monospace";
+                allFontButtons[i].setAttribute("checked", "checked");
+                label1.setAttribute("for", "radio" + i);
+                label1.innerHTML = "<br>" + allFontButtons[i].value + "";
+                document.querySelector("form").appendChild(label1);
+            } //Default Font
+            if (i == 1) {
+                allFontButtons[i].value = "sans-serif";
+                label2.setAttribute("for", "radio" + i);
+                label2.innerHTML = "<br>" + allFontButtons[i].value + "";
+                document.querySelector("form").appendChild(label2);
+            }
+            if (i == 2) {
+                allFontButtons[i].value = "Lucida Sans";
+                label3.setAttribute("for", "radio" + i);
+                label3.innerHTML = "<br>" + allFontButtons[i].value + "";
+                document.querySelector("form").appendChild(label3);
+            }
+            document.querySelector("form").appendChild(allFontButtons[i]);
+        }
+
+    }
+
+
+
+    function delStartMenu(): void {
+        document.querySelector("form").appendChild(delMenu);
+        delMenu.innerHTML = "Start";
+        delMenu.addEventListener("click", startGame);
+    }
+
+
+
+    function startGame(): void {
+        radioEntry();
+        document.querySelector("body").style.backgroundColor = colorSelectBG.value;
+        document.querySelector("body").removeChild(document.querySelector("form"));
+        createCards(parseInt(stepper.value));
+    }
+    function radioEntry(): void {
+        let formData: FormData = new FormData(document.forms[0]);
+        if (formData.get("radio") != null && formData.get("radio") != undefined) {
+            cardFont = (formData.get("radio")) + "";
         }
     }
 
 
 
+
+
+
+
+
+    //Game <-------------------------------------------------------------------->
     function createCards(_amount: number): void {
         let cards: number[] = [];
         for (let i: number = _amount; i > 0; i--) {
             if (i % 2 == 0) {
-                cards.push(i);      //Karte 1
-                cards.push(i);      //Karte 2
+                cards.push(i);      //card 1
+                cards.push(i);      //card 2
             }
         }
         shuffleCards(cards);
@@ -62,11 +221,15 @@ Quellen: <>
     function playCard(_cards: number[], _randomNumber: number): void {
         let card: HTMLDivElement = document.createElement("div");
         card.className = _cards[_randomNumber] + "";
+        card.style.padding = cardSize + "px";
+        card.style.fontFamily = cardFont + "";
+        card.style.color = colorSelectCard.value;
+        card.style.backgroundColor = colorSelectCard.value;
         document.querySelector("#wrapper").appendChild(card);
         card.addEventListener("click", clickCard);
-        card.setAttribute("hidden", "true");
+        card.style.backgroundColor = colorSelectCard.value;
         if (_cards[_randomNumber] < 10) { card.innerHTML = "0" + _cards[_randomNumber] + ""; }
-        else {card.innerHTML = _cards[_randomNumber] + ""; }
+        else { card.innerHTML = _cards[_randomNumber] + ""; }
     }
 
 
@@ -78,12 +241,13 @@ Quellen: <>
         }
         clicked++;
         setTimeout(click, 2000);
-        this.setAttribute("hidden", "false");
+        this.style.backgroundColor = "lightblue";
+        this.style.color = colorSelectFont.value;
         toCheck.push(this.className);
     }
 
 
-    
+
     function click(): void {
 
         if (clicked == 2) {
@@ -93,7 +257,8 @@ Quellen: <>
             if (checkOne == checkTwo) {
                 isMatch(checkOne);
             }
-            else {noMatch(checkOne, checkTwo);
+            else {
+                noMatch(checkOne, checkTwo);
             }
             toCheck = [];
             clicked = 0;
@@ -117,17 +282,25 @@ Quellen: <>
 
 
     function noMatch(_checkOne: number, _checkTwo: number): void {
-            let elementsCheckOne: HTMLCollectionOf<Element> = document.getElementsByClassName(_checkOne + "");
-            elementsCheckOne[0].removeAttribute("noninteraction");
-            elementsCheckOne[1].removeAttribute("noninteraction");
-            elementsCheckOne[0].setAttribute("hidden", "true");
-            elementsCheckOne[1].setAttribute("hidden", "true");
-            let elementsCheckTwo: HTMLCollectionOf<Element> = document.getElementsByClassName(_checkTwo + "");
-            elementsCheckTwo[0].removeAttribute("noninteraction");
-            elementsCheckTwo[1].removeAttribute("noninteraction");
-            elementsCheckTwo[0].setAttribute("hidden", "true");
-            elementsCheckTwo[1].setAttribute("hidden", "true");
-        }
+        let elementsCheckOne: HTMLCollectionOf<Element> = document.getElementsByClassName(_checkOne + "");
+        elementsCheckOne[0].removeAttribute("noninteraction");
+        elementsCheckOne[1].removeAttribute("noninteraction");
+        let elOne1: HTMLElement = <HTMLElement>elementsCheckOne[0];
+        elOne1.style.backgroundColor = colorSelectCard.value;
+        elOne1.style.color = colorSelectCard.value;
+        let elOne2: HTMLElement = <HTMLElement>elementsCheckOne[1];
+        elOne2.style.backgroundColor = colorSelectCard.value;
+        elOne2.style.color = colorSelectCard.value;
+        let elementsCheckTwo: HTMLCollectionOf<Element> = document.getElementsByClassName(_checkTwo + "");
+        elementsCheckTwo[0].removeAttribute("noninteraction");
+        elementsCheckTwo[1].removeAttribute("noninteraction");
+        let elTwo1: HTMLElement = <HTMLElement>elementsCheckTwo[0];
+        elTwo1.style.backgroundColor = colorSelectCard.value;
+        elTwo1.style.color = colorSelectCard.value;
+        let elTwo2: HTMLElement = <HTMLElement>elementsCheckTwo[1];
+        elTwo2.style.backgroundColor = colorSelectCard.value;
+        elTwo2.style.color = colorSelectCard.value;
+    }
 
 
 
